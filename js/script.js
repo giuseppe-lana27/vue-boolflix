91f59a1f4696bb5e63c3aeb6c787acb3325e9783
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-04-09 15:55:42 +0200

    inserita funzione per ricerca dei film

--- js/script.js
+++ js/script.js
@@ -2,9 +2,24 @@ var app = new Vue(
   {
     el: '#app',
     data:{
-
+      films: [],
+      filmTitle: '',
     },
     methods:{
-      
+      searchFilm: function(){
+        axios.get('https://api.themoviedb.org/3/search/movie?',{
+          params: {
+            api_key: '2af3dbe2dc67db19a015c5cf931f2864',
+            query: this.filmTitle,
+            language: 'it-IT',
+          }
+        })
+        .then((risposta) =>{
+          // console.log(risposta.data);
+          this.films = risposta.data.results;
+          // console.log(this.films);
+          this.filmTitle = '';
+        })
+      }
     }
 });
