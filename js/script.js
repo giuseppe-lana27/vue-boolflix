8ef8bf68cf856d1a34ebe818203c3e6f6dd32c0e
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-04-13 15:58:43 +0200

    inserita visualizzazione stella vuota in voto

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@ var app = new Vue(
   {
     el: '#app',
     data:{
+      apiKey: '2af3dbe2dc67db19a015c5cf931f2864',
+      apiUrl: 'https://api.themoviedb.org/3/search/',
       films: [],
       search: '',
-      tvShows: [],      
+      tvShows: [],
     },
     methods:{
       searchFilm: function(){
-        axios.get('https://api.themoviedb.org/3/search/movie?',{
+        axios.get(this.apiUrl + 'movie',{
           params: {
-            api_key: '2af3dbe2dc67db19a015c5cf931f2864',
+            api_key: this.apiKey,
             query: this.search,
             language: 'it-IT',
           }
@@ -22,9 +24,9 @@ var app = new Vue(
             this.films[i].vote_average = Math.ceil(this.films[i].vote_average / 2);
           }
         })
-        axios.get('https://api.themoviedb.org/3/search/tv?',{
+        axios.get(this.apiUrl + 'tv',{
           params: {
-            api_key: '2af3dbe2dc67db19a015c5cf931f2864',
+            api_key: this.apiKey,
             query: this.search,
             language: 'it-IT',
           }
